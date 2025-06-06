3e4a7f9c0b9f37692addd6fcc37b15d871acaa55
Dev Pandey <pandeydev894@gmail.com>
2025-06-07 03:58:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ if __name__=="__main__":
     host="0.0.0.0"
     port=int(os.environ.get("PORT",5000))
     print(f"App running on:http://{host}:{port}")
-    app.run(host=host,port=port,debug=False)
+    app.run(host=host,port=port,debug=True)
         
         
